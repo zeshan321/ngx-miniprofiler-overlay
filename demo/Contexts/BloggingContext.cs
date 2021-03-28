927d1f007c0e977bd1f7cb6a01154474305c7b5f
Zeshan Aslam <zeshan12aslam@gmail.com>
2021-03-28 19:00:04 -0400

    Moved blogging db into project

--- demo/Contexts/BloggingContext.cs
+++ demo/Contexts/BloggingContext.cs
@@ -14,7 +14,7 @@ namespace demo.Contexts
         // The following configures EF to create a Sqlite database file as `C:\blogging.db`.
         // For Mac or Linux, change this to `/tmp/blogging.db` or any other absolute path.
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite(@"Data Source=C:\Users\zesha\blogging.db");
+            => options.UseSqlite(@"Data Source=blogging.db");
     }
 
     public class Blog
