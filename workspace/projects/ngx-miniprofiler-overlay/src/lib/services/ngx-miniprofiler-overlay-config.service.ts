0b0c42b61c08e63e0487c3b93a3950db010fd687
Zeshan Aslam <zeshan12aslam@gmail.com>
2021-03-28 02:25:14 -0400

    updated match default

--- workspace/projects/ngx-miniprofiler-overlay/src/lib/services/ngx-miniprofiler-overlay-config.service.ts
+++ workspace/projects/ngx-miniprofiler-overlay/src/lib/services/ngx-miniprofiler-overlay-config.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class NgxMiniprofilerOverlayServiceConfig {
   api = '/mini-profiler-resources';
-  matcher = [];
+  matcher = ['*'];
   thresholds = {
     good: (ms: number) => { return false; },
     okay: (ms: number) => { return false; },
